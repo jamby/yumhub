d68b8b0d3ad4d339797d8ec13005f364fce3001a
Jared Hamby <irjamby@gmail.com>
2013-04-12 21:28:13 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yumhub::Application.config.secret_token = '24b9c0ab8d9af9d65384a39a7947d24074eecf39b585a7ee5442d0e2938b5f6fcc359078c6b1eaa92b2ff22c5af40796f5cf943a189ae4d88ce803b57b223a6a'
