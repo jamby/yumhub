234bbd35a85ecd81dcbecdd592f9ee9ce44484f0
Jared Hamby <irjamby@gmail.com>
2013-04-15 21:55:04 -0400

    Added bootstrap notifications to look pretty when failure occurs. Also changed devise routes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   attr_accessible :login
   attr_accessor :login
   
-  UNUSEABLE_USERNAMES = ["meals", "about"] 
+  UNUSEABLE_USERNAMES = ["meals", "about", "login", "log_in", "signup", "sign_up", "signin", "sign_in", "logout", "log_out", "signout", "sign_out"] 
   
   validate :username_useable?
   validates :username, presence: true, uniqueness: { case_sensitive: true }
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Yumhub::Application.routes.draw do
   
-  devise_for :users
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'signup'}
   
   resources :users, only: [:show], path: ''
 
